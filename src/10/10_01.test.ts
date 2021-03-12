c491b64c54a20c4ad9a8d473b2cb3a02dc837193
muzyk0 <ru9art@gmail.com>
2021-03-12 18:34:34 +0300

    test immutability, spreed operation

--- src/10/10_01.test.ts
+++ src/10/10_01.test.ts
@@ -1,11 +1,6 @@
-import React from 'react';
-import {UserType} from './10_01';
+import {makeHairstyle, moveUser, upgradeUserLaptop, UserType, UserWithLaptopType} from './10_01';
 
 
-
-function hairdresser(u: UserType, power: number) {
-    u.hair = u.hair / power
-}
 
 test('reference type test', () => {
 
@@ -13,12 +8,57 @@ test('reference type test', () => {
         name: 'Dimych',
         hair: 32,
         address: {
-            title: 'Minsk'
+            city: 'Minsk'
         }
     }
 
-    hairdresser(user, 2)
+    const awesomeUser = makeHairstyle(user, 2)
 
-    expect(user.hair).toBe(16)
+    expect(user.hair).toBe(32)
+    expect(awesomeUser.hair).toBe(16)
+    expect(awesomeUser.address).toBe(user.address)
 
+})
+test('change address', () => {
+
+    let user: UserWithLaptopType = {
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'Asus'
+        }
+    }
+
+    const movedUser = moveUser(user, 'Moscow')
+
+    expect(user).not.toBe(movedUser)
+    expect(user.address).not.toBe(movedUser.address)
+    expect(user.laptop).toBe(movedUser.laptop)
+    expect(user.address.city).not.toBe('Kiev')
+})
+
+test('upgrade laptop to macbook', () => {
+
+    let user: UserWithLaptopType = {
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'Asus'
+        }
+    }
+
+    const userCopy = upgradeUserLaptop(user, 'macbook')
+
+    expect(user).not.toBe(userCopy)
+    expect(user.laptop).not.toBe(userCopy.laptop)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.laptop.title).not.toBe('macbook')
 })
--- src/10/10_01.ts
+++ src/10/10_01.ts
@@ -1,6 +1,41 @@
 import React from 'react';
+
 export type UserType = {
     name: string
     hair: number
-    address: { title: string }
+    address: { city: string, house?: number }
+}
+export type LaptopType = {
+    title: string
+}
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+export function makeHairstyle(u: UserType, power: number) {
+    const copy = {
+        ...u,
+        hair: u.hair / power,
+    }
+    /*const copy = {
+        ...u,
+        hair: u.hair / power,
+        address: {...u.address}
+    }*/
+    //copy.hair = u.hair / power
+    return copy
+}
+
+export const moveUser = (user: UserWithLaptopType, city: string) => {
+    return {
+        ...user, //Shallow copy
+        address: {...user.address, title: city}
+    }
+}
+
+export const upgradeUserLaptop = (user: UserWithLaptopType, laptop: string) => {
+    return {
+        ...user,
+        laptop: {...user.laptop, laptop: laptop}
+    }
 }
